0fc7491fe6b13c71ea770ef9c525917d48d869c7
Chibuike <aniakuchibuike@gmail.com>
2023-01-27 13:56:21 +0100

    tasks

--- index.js
+++ index.js
@@ -1,62 +1,126 @@
+
 import { Builder, By, Key, until, Browser } from 'selenium-webdriver';
-import { expect, assert} from 'chai';
-import LoginPage from './Pages/LoginPage'
+const { expect, assert} = require('chai');
 
 // const chrome = require('selenium-webdriver/chrome');
 // const chromedriver = require('chromedriver')
-require('dotenv').config()
+import { config}  from 'dotenv'
+import { alertIsPresent, elementIsVisible } from 'selenium-webdriver/lib/until';
 
+
+const driver = new Builder().forBrowser('firefox').build();
 
 describe('End to End Test Suite', () => {
-    const login = new LoginPage()
+   
 
     beforeEach('should launch',async () =>{
       try {
-        await  login.launcher()
-        await driver.getTitle().then(value => {
-          console.log('your title is:', value);
-        });
+        await  driver.get('https://demoblaze.com')
       } catch (error) {
         console.log(error);
       }    
     }) 
 
-    it('login a user', async()=>{
-      try {
-        login.getUsername('username')
-         await driver.findElement(By.id('username')).getAttribute('id').then(ids => {
-    
-        console.log('your attribute is',ids );   
-        assert.deepEqual(ids,'username')     
-        })
-        await login.inputPassword('password')
-            await driver.findElement(By.name('password'))  .getAttribute('name').then(name=>{
-                console.log('yoursecond attribute is', name);
-                assert.deepEqual(name, 'password')
+    it('should be able to order using AMEX credit card', async() => {
+        try {
+            await driver.findElement(By.xpath("(//a[@id='itemc'])[2]")).click()
+            await driver.wait(until.elementLocated(By.linkText("MacBook Pro")),11000).click();
+            await driver.wait(until.elementLocated(By.xpath(`//a[@class='btn btn-success btn-lg']`)),11000).click();
+            await driver.wait(alertIsPresent(),4000)
+            const alert = driver.switchTo().alert()
+            await alert.dismiss()
+            await driver.wait(until.elementLocated(By.linkText("Cart")),11000).click();
+            await driver.sleep(6000)
+            const totalPrice = await driver.wait(until.elementLocated(By.xpath(`//h3[@class='panel-title']`))).getText()
+            await driver.findElement(By.xpath("(//button[@class='btn btn-success'])")).click()
+            await driver.findElement(By.id('name')).sendKeys('John Doe')
+            await driver.findElement(By.id('country')).sendKeys('Portugal')
+            await driver.findElement(By.id('city')).sendKeys('Lisbon')
+            await driver.findElement(By.id('card')).sendKeys('375567668884617')
+            await driver.findElement(By.id('month')).sendKeys('02')
+            await driver.findElement(By.id('year')).sendKeys('2030')
+            await driver.findElement(By.xpath("(//button[@class='btn btn-primary'])[3]")).click()
+            await driver.wait(until.elementLocated(By.xpath(`//p[@class='lead text-muted ']`)),11000).getText()
+            .then(value => {
+              const idValue = value.split('\n')[0]
+              const idDivider = idValue.split(' ')
+              const realId = idDivider[1]
+              expect(Number.parseInt(realId)).to.be.a('number')
+
+              const amountValue = value.split('\n')[1]
+              const amountDivider = amountValue.split(' ')
+              const realAmount = amountDivider[1]
+              expect(realAmount).to.equal(totalPrice)
+
+              const nameValue = value.split('\n')[3]
+              const realName = nameValue.split(' ').slice(-2).join(' ')
+              expect(realName).to.equal('John Doe')
             })
-         await driver.findElement(By.xpath(`//button[@class='radius']`)).click()
-      } catch (error) {
-        console.log(error);
-      }
-      
+            
+        } catch (error) {
+            console.log(error);
+        }
     })
 
+    it('should Place order using VISA credit card', async () => {
+        try {
+            await driver.findElement(By.xpath("(//a[@id='itemc'])[2]")).click()
+            await driver.wait(until.elementLocated(By.linkText("MacBook Pro")),11000).click();
+            await driver.wait(until.elementLocated(By.xpath(`//a[@class='btn btn-success btn-lg']`)),11000).click();
+            await driver.wait(alertIsPresent(),4000)
+            const alert = driver.switchTo().alert()
+            await alert.dismiss()
+            await driver.wait(until.elementLocated(By.linkText("Cart")),11000).click();
+            await driver.sleep(6000)
+            const totalPrice = await driver.wait(until.elementLocated(By.xpath(`//h3[@class='panel-title']`))).getText()
+            await driver.findElement(By.xpath("(//button[@class='btn btn-success'])")).click()
+            await driver.findElement(By.id('name')).sendKeys('Percy Clayton')
+            await driver.findElement(By.id('country')).sendKeys('United States')
+            await driver.findElement(By.id('city')).sendKeys('Jacksonville')
+            await driver.findElement(By.id('card')).sendKeys('4411732769254916')
+            await driver.findElement(By.id('month')).sendKeys('44')
+            await driver.findElement(By.id('year')).sendKeys('2029')
+            await driver.findElement(By.xpath("(//button[@class='btn btn-primary'])[3]")).click()
+            await driver.wait(until.elementLocated(By.xpath(`//p[@class='lead text-muted ']`)),11000).getText()
+            .then(value => {
+              const idValue = value.split('\n')[0]
+              const idDivider = idValue.split(' ')
+              const realId = idDivider[1]
+              expect(Number.parseInt(realId)).to.be.a('number')
 
+              const amountValue = value.split('\n')[1]
+              const amountDivider = amountValue.split(' ')
+              const realAmount = amountDivider[1]
+              expect(realAmount).to.equal(totalPrice)
 
-    // it('it should sign up a user with inputs', async ()=>{
-    //   try {
-    //     await driver.findElement(By.xpath(`//button[@title='Account']`)).click();
-    //     await driver.findElement(By.xpath(`(//a[@class='pz-btn-group__btn'])[2]`)).click();
-    //     // await driver.wait(until.elementLocated(By.className('font-weight-semi-bold'),4000)).getText().then(value =>{
-    //     //   console.log('your value is:',value);
-    //     //   assert.deepEqual(value,'Login')
-    //     // })
-    //     await driver.wait(until.elementLocated(By.xpath(`//input[@name='email']`)),2000).sendKeys("chibuiky@gmail.com");
-    //     await driver.findElement(By.xpath(`//input[@name='password']`)).sendKeys("password");
-    //     await driver.wait(until.elementLocated(By.xpath(`//button[@class='pz-btn pz-btn--lg position-relative pz-btn--primary pz-btn--wide']`)),2000).click();
-        
-    //   } catch (error) {
-    //     console.log(error);
-    //   }
-    // })
+              const nameValue = value.split('\n')[3]
+              const realName = nameValue.split(' ').slice(-2).join(' ')
+              expect(realName).to.equal('Percy Clayton')
+            })
+        } catch (error) {
+          console.log(error);
+        }
+    })
+
+    it('should not place an order without a credit card', async () => {
+        try {
+          await driver.findElement(By.xpath("(//a[@id='itemc'])[2]")).click()
+            await driver.wait(until.elementLocated(By.linkText("MacBook Pro")),11000).click();
+            await driver.wait(until.elementLocated(By.xpath(`//a[@class='btn btn-success btn-lg']`)),11000).click();
+            await driver.wait(alertIsPresent(),4000)
+            const alert = driver.switchTo().alert()
+            await alert.dismiss()
+            await driver.wait(until.elementLocated(By.linkText("Cart")),11000).click();
+            await driver.sleep(4000)
+            await driver.findElement(By.xpath("(//button[@class='btn btn-success'])")).click()
+            await driver.findElement(By.xpath("(//button[@class='btn btn-primary'])[3]")).click()
+            const newAlert = driver.switchTo().alert()
+            const alertValue = await newAlert.getText()
+            console.log('am here:', alertValue);
+            assert.deepEqual(alertValue,'Please fill out Name and Creditcard.')
+        } catch (error) {
+          console.log(error);
+        }
+    })
+
 })
